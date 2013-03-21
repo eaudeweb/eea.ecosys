364dc35f6a5da81da46b57457e90b4c0ad18b2a9
Dragos Catarahia <dragos.catarahia@gmail.com>
2013-03-21 13:07:41 +0200

    mongo integration

--- ecosys/app.py
+++ ecosys/app.py
@@ -1,11 +1,14 @@
 import flask
+from models import db
 
 # import blueprints
 from .library import library
 
 
 DEFAULT_CONFIG = {
-
+    'MONGODB_SETTINGS': {
+        'DB': 'ecosys',
+    }
 }
 
 BLUEPRINTS = (
@@ -13,11 +16,14 @@ BLUEPRINTS = (
 )
 
 
+
+
 def create_app(instance_path=None, config={}):
     app = flask.Flask(__name__, instance_path=instance_path,
                       instance_relative_config=True)
     configure_app(app, config)
     configure_blueprints(app, BLUEPRINTS)
+    db.init_app(app)
     return app
 
 
@@ -30,5 +36,3 @@ def configure_app(app, config):
 def configure_blueprints(app, blueprints):
     for blueprint in blueprints:
         app.register_blueprint(blueprint)
-
-
--- ecosys/models.py
+++ ecosys/models.py
@@ -1,2 +1,8 @@
+from flask.ext.mongoengine import MongoEngine
 
 
+db = MongoEngine()
+
+
+class Authors(db.Document):
+    name = db.StringField(max_length=128)
