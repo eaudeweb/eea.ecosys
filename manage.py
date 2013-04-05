980e5ef1d81ec45dcc561c1aaa31f474204d6e2a
Dragos Catarahia <dragos.catarahia@gmail.com>
2013-04-05 12:24:26 +0300

    fix bug for env

--- manage.py
+++ manage.py
@@ -12,7 +12,7 @@ from ecosys.app import create_app
 PROJECT_ROOT = os.environ.get('PROJECT_ROOT', path(__file__).parent.abspath())
 
 
-app = create_app(instance_path=PROJECT_ROOT / 'instance')
+app = create_app(instance_path=path(PROJECT_ROOT) / 'instance')
 app.wsgi_app = ReverseProxied(app.wsgi_app)
 manager = script.Manager(app)
 
