8920a29051a9b314654eb43de3a75b32e2856da6
Dragos Catarahia <dragos.catarahia@gmail.com>
2013-04-05 12:20:46 +0300

    PROJECT_ROOT from evn

--- manage.py
+++ manage.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
+import os
+
 from flask.ext import script
-from middlewares import ReverseProxied
 from path import path
 
+from middlewares import ReverseProxied
 from ecosys.app import create_app
 
 
-PROJECT_ROOT = path(__file__).parent.abspath()
+PROJECT_ROOT = os.environ.get('PROJECT_ROOT', path(__file__).parent.abspath())
 
 
 app = create_app(instance_path=PROJECT_ROOT / 'instance')
