7eafe9b79d24282c3fca1b611350d65e022d7f52
Dragos Catarahia <dragos.catarahia@gmail.com>
2013-04-05 11:08:53 +0300

    ReverseProxy middleware

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from flask.ext import script
+from middlewares import ReverseProxied
 from path import path
 
 from ecosys.app import create_app
@@ -10,6 +11,7 @@ PROJECT_ROOT = path(__file__).parent.abspath()
 
 
 app = create_app(instance_path=PROJECT_ROOT / 'instance')
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 manager = script.Manager(app)
 
 
