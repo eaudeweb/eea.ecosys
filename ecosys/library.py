30fa26f35133c303362e3e724490f61495b82051
Mihnea Simian <contact@mesimian.com>
2013-04-29 10:43:11 +0300

    improved profile; profile required before contribute

--- ecosys/library.py
+++ ecosys/library.py
@@ -38,6 +38,11 @@ class Edit(views.MethodView):
 
     def get(self, resource_type):
         resource_form, review_form = self._get_instance_form(resource_type)
+        user = flask_login.current_user
+        if not user.is_anonymous() and not user.country:
+            resp = redirect(url_for('auth.profile',
+                            next=url_for('.edit', resource_type=resource_type)))
+            return resp
         return render_template('edit.html', resource_form=resource_form,
                                review_form=review_form)
 
